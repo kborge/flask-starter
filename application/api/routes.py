6e1fa18157051b0db6d632a25f4e481fda98e9fb
Kerwin Bonn Manisan <kmanisan@isolara.com>
2018-05-13 23:20:41 +0800

    added login/logout and system requirements.txt

--- application/api/routes.py
+++ application/api/routes.py
@@ -1,8 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response, jsonify
+from flask_cors import CORS, cross_origin
 
 api = Blueprint('api', __name__)
+CORS(api)
 
 
-@api.route('/')
+@api.route('/', methods=['GET'])
+@cross_origin()
 def index():
-    return '{"results" : "Hello World"}'
+    data = {'data': 'hello coder!'}
+    return make_response(jsonify(data), 200)
+
+
+@api.route('/login', methods=['POST'])
+@cross_origin()
+def login():
+    # username = request.form.get('username')
+    # password = request.form.get('password')
+    return 'Welcome Back'
+
+
+@api.route('/logout', methods=['GET'])
+@cross_origin()
+def logout():
+    return 'See you again!'
